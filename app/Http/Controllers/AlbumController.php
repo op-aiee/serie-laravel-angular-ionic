de503bafa56aa25a0ecb5ceba48cca69157b0793
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-04-27 18:04:58 -0300

    api laravel init

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Album;
+use Illuminate\Http\Request as Request;
+use Illuminate\Http\Response as Response;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Album::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Album::create(['name' => $request->name]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Album $album)
+    {
+        return $album;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Album $album)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        //
+    }
+}
